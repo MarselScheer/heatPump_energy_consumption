dd294b8bbc128339123313ed022477886e11a7d5
m <scheer@freescience.de>
2019-09-29 15:08:53 +0200

    assure that input-element for temperature is not an integer

--- ui.R
+++ ui.R
@@ -16,7 +16,7 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       textInput("file_save", "Save location:", value = "/srv/data/power_by_time.csv"),
-      numericInput("temperature_outside", label = "Temperature outside:", value = 0.0, min = -80, max = 80, step = 0.1),
+      numericInput("temperature_outside", label = "Temperature outside:", value = 0.1, min = -80, max = 80, step = 0.1),
       numericInput("power_indicator", label = "Value of power indicator:", value = 12000, min = 0, max = Inf, step = 1),
       textInput("time", "Time:", value = lubridate::now()),
       actionButton("update_time_to_now", label = "Update Time"),
@@ -35,6 +35,9 @@ shinyUI(fluidPage(
           dataTableOutput("power_indicator_by_time")),
         tabPanel(
           "Changelog", 
+          
+          h1("v0.1.2"),
+          p("- input element for temperature is not initialized with an integer (in order to gurantee that a decimal point is available) "),
           
           h1("v0.1.1"),
           p("- temperature outside, power indicator and heatpump settings are initialized with the latest stored values"),
