6c052a56c047fdd6d5d65920501f6e7b7c3d7622
m <scheer@freescience.de>
2021-10-03 10:10:27 +0200

    add missing package to Dockerfile

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-sudo docker run --user shiny -d -p 80:3838 \
+sudo docker run --user shiny --rm -d -p 80:3838 \
      -v /srv/shinyapps/:/srv/shiny-server/ -v /tmp/:/var/log/shiny-server/ \
      -v /mnt/samba/data/m/shinyApps/heatpump/:/srv/data/ heat_pump
