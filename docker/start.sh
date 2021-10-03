657ec02ca0abf2f6c08a013aa1a53c7a920cc8e1
m <scheer@freescience.de>
2021-10-03 10:19:01 +0200

    update version in docker-start-command

--- docker/start.sh
+++ docker/start.sh
@@ -2,4 +2,4 @@
 
 sudo docker run --user shiny --rm -d -p 80:3838 \
      -v /srv/shinyapps/:/srv/shiny-server/ -v /tmp/:/var/log/shiny-server/ \
-     -v /mnt/samba/data/m/shinyApps/heatpump/:/srv/data/ heat_pump
+     -v /mnt/samba/data/m/shinyApps/heatpump/:/srv/data/ heat_pump:0.2.0
