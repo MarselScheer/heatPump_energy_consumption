c7c20193ea6816dd247d18062682e7f6eca32ed9
Marsel Scheer <scheer@freescience.de>
2021-10-03 10:30:37 +0200

    prepare next-dev-cycle

--- docker/start.sh
+++ docker/start.sh
@@ -2,4 +2,4 @@
 
 sudo docker run --user shiny --rm -d -p 80:3838 \
      -v /srv/shinyapps/:/srv/shiny-server/ -v /tmp/:/var/log/shiny-server/ \
-     -v /mnt/samba/data/m/shinyApps/heatpump/:/srv/data/ heat_pump:0.2.0
+     -v /mnt/samba/data/m/shinyApps/heatpump/:/srv/data/ heat_pump:0.2.0.9000
